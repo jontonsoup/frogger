5a2ff0da3fd8ca764cbc3494c84b0b329bcef821
Jonathan Friedman <friedmanj98@gmail.com>
2013-05-03 18:02:28 +0200

    collision starting to work

--- frogger.rb
+++ frogger.rb
@@ -1,4 +1,4 @@
-Shoes.app(:width => 500, :height => 500, :resizable => false) do
+Shoes.app(:width => 550, :height => 550, :resizable => false) do
   init
 
   move_position = 20
@@ -6,6 +6,7 @@ Shoes.app(:width => 500, :height => 500, :resizable => false) do
   @bottom = 500
   @right = 500
   @left = 0
+  collision_size = 20
 
 
   keypress do |k|
@@ -22,6 +23,7 @@ Shoes.app(:width => 500, :height => 500, :resizable => false) do
       @frog.move(@frog.left - move_position, @frog.top )
     end
   end
+
   animate(car_speed) do
     @right_moving_cars.each do |car|
       if @right < car.left
@@ -29,12 +31,19 @@ Shoes.app(:width => 500, :height => 500, :resizable => false) do
       else
         car.move(car.left + move_position, car.top)
       end
+      if car.top == @frog.top && car.left == @frog.left
+        alert("You lose!")
+      end
     end
     @left_moving_cars.each do |car|
       if @left > car.left
         car.move(@right, car.top)
       else
         car.move(car.left - move_position, car.top)
+      end
+      if car.top + collision_size > @frog.top && car.top - collision_size < @frog.top && car.left + collision_size > @frog.left && car.left - collision_size < @frog.left
+
+        alert("top: car " + (car.left + collision_size).to_s + " top frog: " + @frog.left.to_s + " " + (car.left - collision_size).to_s + " " + @frog.left.to_s)
       end
     end
   end
@@ -48,16 +57,18 @@ def init
   (0..20).each do |i|
     flow {line 0,i * 60, width, i * 60}
   end
-  @frog = oval 10, 400, 40
+  @frog = oval 10, 530, 40
 
   @right_moving_cars = []
-  (0..5).each do |i|
-    @right_moving_cars << rect(@left, i * car_spacing, 50, 10)
+  (0..4).each do |i|
+    offset = i * 300 * rand
+    @right_moving_cars << rect(offset, i * car_spacing, 50, 10)
   end
 
   @left_moving_cars = []
-  (0..5).each do |i|
-    @left_moving_cars << rect(@right, i * car_spacing + 30, 50, 10)
+  (0..4).each do |i|
+    offset = i * 300 * rand
+    @left_moving_cars << rect(offset, i * car_spacing + 30, 50, 10)
   end
 end
 
